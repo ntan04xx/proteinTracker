9c2ee74913e719cc20903d5504b8657e89d92ca1
ntan04xx <nathantan04@gmail.com>
2024-06-02 22:54:06 +1000

    Started progress tracker

--- proteinWorld/proteinTracker/calorie_count/forms.py
+++ proteinWorld/proteinTracker/calorie_count/forms.py
@@ -1,5 +1,32 @@
 from django import forms
 
 class ApiRequestForm(forms.Form):
-    input_food = forms.CharField(widget = forms.Textarea(attrs = {'placeholder': 'Enter Data Here '}), label = "f")
+    input_food = forms.CharField(widget = forms.Textarea(attrs = {'placeholder': 'Enter Data Here '}), label = "")
     input_amount = forms.CharField(widget = forms.Textarea(attrs = {'placeholder': 'Enter Data Here '}), label = "")
+
+class TargetRequestForm(forms.Form):
+    age = forms.IntegerField(widget = forms.Textarea(attrs = {'placeholder': 'Enter Data Here '}), label = "")
+    height = forms.FloatField(widget = forms.Textarea(attrs = {'placeholder': 'Enter Data Here '}), label = "")
+    weight = forms.FloatField(widget = forms.Textarea(attrs = {'placeholder': 'Enter Data Here '}), label = "")
+
+    GENDER = [
+        ('M', 'Male'),
+        ('F', 'Female')
+    ]
+    gender = forms.ChoiceField(choice = GENDER, widget = forms.RadioSelect)
+
+    ACTIVITY = [
+        ('None', '0 times per week'),
+        ('Low', '1-2 times per week'),
+        ('Medium', '3-4 times per week'),
+        ('High', '>5 times per week')
+    ]
+    activity = forms.ChoiceField(choice = ACTIVITY, widget = forms.RadioSelect)
+
+    GOAL = [
+        ('Cut', 0.1)
+        ('Cut', 0.2),
+        ('Bulk', 0.1),
+        ('Bulk', 0.2)
+    ]
+    goal = forms.ChoiceField(choice = GOAL, widget = forms.RadioSelect)
--- proteinWorld/proteinTracker/calorie_count/urls.py
+++ proteinWorld/proteinTracker/calorie_count/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('api_request/', views.api_request_view, name='api_request'),
     path('', views.home_page, name = "home")
+    #path('target_request/', views.target_request_view, name = "target_request")
 ]
