e9b5a7596421d18755752be3b59f4f319ab4cad3
ntan04xx <nathantan04@gmail.com>
2024-06-15 00:49:25 +1000

    Turned off debug mode

--- proteinWorld/proteinTracker/proteinTracker/settings.py
+++ proteinWorld/proteinTracker/proteinTracker/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-%1xtwevj0bl^b#g_)0c29rjqsv3lunzk=0v=&m$ent8&mmgc0+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
